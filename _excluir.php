af0ea7bf99885a39502f3b141f664936daf85cbc
Keera <81645001+Ailee-pixel@users.noreply.github.com>
2022-08-03 11:54:08 -0300

    Add files via upload

--- _excluir.php
+++ _excluir.php
@@ -1,27 +1,22 @@
 <?php
 require_once("conexao.php");
 
-$id = $_GET['id'];
+$id = $_POST['id'];
+$deletar = "DELETE FROM estudante WHERE id = $id";
+$excluir = mysqli_query($conexao, $deletar);
 
-$query = "DELETE FROM alunos WHERE id = $id";
-$resultado = mysqli_query($conexao, $query);
-$select_usuario = mysqli_fetch_assoc($resultado);
+if(mysqli_affected_rows($conexao)){
+    HEADER("Location:frmconsultar.php");
+    }else{
+        echo "ERRO AO EXCLUIR";
+    }
+    mysqli_close($conexao);
+     
+
+
+
+
+
+
+
 ?>
-
-<!DOCTYPE html>
-    <html lang="pt-br">
-      <head>
-        <title>EXCLUIR DADOS</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h2>EXCLUIR DADOS</h2>
-        <BR>
-        <form action="_delete.php" method="POST">
-          <input type="hidden" name = "id" value = "<?php echo $select_usuario['id']?>"><br>  
-          <label for="nome">Nome Do Estudante:<input type="text" name ="nome" value = "<?php echo $select_usuario['nome']?>" disabled></label><br>
-          <label for="idade">Idade do Estudante:<input type="text" name = "idade" value = "<?php echo $select_usuario['idade']?>" disabled></label><br>
-          <input type = "submit" value="Excluir">&nbsp;&nbsp;<a href = 'frmConsultar.php'>Voltar</a>
-    
-      </body>
-    </html>
---
+++ frmExcluir.php
@@ -0,0 +1,27 @@
+<?php
+require_once("conexao.php");
+
+$id = $_GET['id'];
+
+$query = "SELECT * FROM estudante WHERE id = $id";
+$resultado = mysqli_query($conexao, $query);
+$select_usuario = mysqli_fetch_assoc($resultado);
+?>
+
+<!DOCTYPE html>
+    <html lang="pt-br">
+      <head>
+        <title>EXCLUIR DADOS</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h2>EXCLUIR DADOS</h2>
+        <BR>
+        <form action="_excluir.php" method="POST">
+          <input type="hidden" name = "id" value = "<?php echo $select_usuario['id']?>"><br>  
+          <label for="nome">Nome Do Estudante:<input type="text" name ="nome" value = "<?php echo $select_usuario['nome']?>" disabled></label><br>
+          <label for="idade">Idade do Estudante:<input type="text" name = "idade" value = "<?php echo $select_usuario['idade']?>" disabled></label><br>
+          <input type = "submit" value="Excluir">&nbsp;&nbsp;<a href = 'frmConsultar.php'>Voltar</a>
+    
+      </body>
+    </html>
